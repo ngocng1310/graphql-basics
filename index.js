46dbd162f5d9749d44f187f6ce72ee334cdc9c4a
ngocng <ngoc.nguyen1@gbst.com>
2018-11-27 15:46:00 +1000

    create a schema

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-import express from 'express';
+const express = require('express');
+//import express from 'express';
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema') ;
 
 const app  = express();
 
 app.get('/', (req, res) => {
     res.send('GraphQL is great!');
 });
+
+const root = { hello: () => "Hi, I'm Ngoc" };
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 
 app.listen(8080, () => console.log('Running server on port localhost:8080/graphql'));
