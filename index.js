6cf6a316977d1eefb5e41b2ebca7611caeec96ea
ngocng <ngoc.nguyen1@gbst.com>
2018-12-03 15:56:21 +1000

    mongo & sequelize

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-const express = require('express');
-//import express from 'express';
-const graphqlHTTP = require('express-graphql');
-const schema = require('./schema') ;
+require('babel-register')({
+    presets: [ 'env' ]
+})
 
-const app  = express();
-
-app.get('/', (req, res) => {
-    res.send('GraphQL is great!');
-});
-
-const root = { hello: () => "Hi, I'm Ngoc" };
-
-app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-}));
-
-app.listen(8080, () => console.log('Running server on port localhost:8080/graphql'));
+// Import the rest of our application.
+module.exports = require('./server.js')
